d5931b79410771ff33176ea0ae2282ee990d8be8
Kerry Russo <kerry.kim.russo@gmail.com>
2017-11-17 18:55:32 -0500

    change numComments to optional

--- src/pages/post.page.js
+++ src/pages/post.page.js
@@ -20,7 +20,7 @@ export default class PostPage extends Component {
         voteScore: PropTypes.number.isRequired,
         category: PropTypes.string.isRequired,
         deleted: PropTypes.bool.isRequired,
-        numComments: PropTypes.number.isRequired,
+        numComments: PropTypes.number,
       }).isRequired,
     comments: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.string.isRequired,
