d3d5ecb04a368a6c0d4e0265c2e5104bebf1353e
Kerry Russo <kerry.kim.russo@gmail.com>
2017-11-17 18:54:16 -0500

    use sortBy state prop to sort posts on home page

--- src/containers/home.page.container.js
+++ src/containers/home.page.container.js
@@ -3,18 +3,18 @@ import HomePage from '../pages/home.page';
 import Actions from '../actions';
 import { 
   filterByCategoryThenSortByVoteScore, 
-  sortPostsByVoteScore, 
+  sortPostsByPropDesc, 
   countCommentsPerPostId, 
   setNumCommentsForPosts 
 } from '../utils';
 
 const mapStateToProps = (state, ownProps) => {
-  const { categories, posts, comments } = state;
+  const { categories, posts, comments, sortBy } = state;
   const { category } = ownProps.match.params;
 
   const filteredPosts = category ? 
     filterByCategoryThenSortByVoteScore(category)(posts) : 
-    sortPostsByVoteScore(posts);
+    sortPostsByPropDesc(sortBy)(posts);
   
   const numCommentsPerPostId = countCommentsPerPostId(comments);
   
@@ -27,6 +27,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => ({
   onPostUpvote: (id) => dispatch(Actions.Post.upvote(id)),
   onPostDownvote: (id) => dispatch(Actions.Post.downvote(id)),
+  onSortChange: (sortBy) => dispatch(Actions.Sort.change(sortBy)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
